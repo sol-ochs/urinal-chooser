a6a80078dbe863cca08e54048fc0a21ce8ffb9af
sochs-olo <106677464+sochs-olo@users.noreply.github.com>
2025-09-28 18:34:04 -0400

    Validate not empty before passing to chooseUrinal

--- Program.fs
+++ Program.fs
@@ -1,20 +1,31 @@
 ﻿open System
 open UrinalLogic
 
+let noUrinalsMessages = [
+    "There are no urinals. Are you sure this is not a closet?"
+    "There are no urinals. Are you in the women's bathroom?"
+]
+
 Console.WriteLine("Enter a row of urinals as 1s and 0s with 1s indicating 'Occupied' and 0s indicating 'Unoccupied': ")
 let row =
     Console.ReadLine()
     |> Seq.map (fun c -> if c = '0' then 0 else 1)
     |> List.ofSeq
 
-// print row
-for urinal in row do
-    match urinal with
-    | 0 -> printf "[ ]"
-    | _ -> printf "[x]"
-printfn ""
+// TODO: Do more input validation
+match row with
+| [] ->
+    let random = Random()
+    let message = noUrinalsMessages.[random.Next(noUrinalsMessages.Length)]
+    printfn "%s" message
+    exit 0
+| _ ->
+    for urinal in row do
+        match urinal with
+        | 0 -> printf "[ ]"
+        | _ -> printf "[x]"
+    printfn ""
 
-try
     match chooseUrinal row with
     | Some bestUrinal ->
         for urinal in [ 0 .. row.Length-1 ] do
@@ -24,5 +35,3 @@ try
         printfn ""
     | None -> 
         printfn "There are no urinals available. Go find a water bottle or something."
-with
-    | :? System.ArgumentException as e -> printfn "Error: %s" e.Message
